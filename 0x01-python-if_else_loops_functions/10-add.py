1a8dc4659217016e729dbd23d09aa37aee33c63e
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-12-08 02:47:47 -0800

    Add numbers

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def add(a, b):
-    result = a + b
-    return abs(result)
+    if (a < 0 and b < 0):
+        result = (a + b)
+    else:
+        result = a + b
+    return (result)
