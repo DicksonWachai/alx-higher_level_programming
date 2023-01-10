4e436fc6fe950f966308ca2c8e4c936be6744971
wachaidickson@gmail.com <wachaidickson@gmail.com>
2023-01-10 04:15:49 -0800

    Correct output error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@
 def inherits_from(obj, a_class):
 
     """Returns a Boolean if class is inherited"""
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
