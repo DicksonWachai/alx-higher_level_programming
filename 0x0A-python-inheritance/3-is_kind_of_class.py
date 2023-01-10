72a6e88c26d5bba58156f87e7847dafec262db06
wachaidickson@gmail.com <wachaidickson@gmail.com>
2023-01-10 02:13:41 -0800

    Same Class or Inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Checks to see if object is part of class"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """Returns a Boolean from isinstance"""
+    return isinstance(obj, a_class)
