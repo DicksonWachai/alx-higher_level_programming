c9bfd76d5f237e081d397cea96d1a52b7c904ebf
wachaidickson@gmail.com <wachaidickson@gmail.com>
2023-01-10 01:15:49 -0800

    Correct Exception Error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,6 +7,6 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a**b) / i
-        except:
+        except IndexError:
             result = b + a
     return result
