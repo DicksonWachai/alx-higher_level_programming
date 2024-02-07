2ba8fb8455f4b7c1aa1f215927e7bd0770a2ca1c
DicksonWachai <wachaidickson@gmail.com>
2024-02-07 12:36:26 +0300

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""JSON representation"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """function returns the JSON representation of an object"""
+    return json.dumps(my_obj)
