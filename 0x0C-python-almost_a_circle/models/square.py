7251781cec9008f5371f7773768fe8b5a5b1c95e
DicksonWachai <wachaidickson@gmail.com>
2024-02-12 19:50:47 +0300

    Correct checker error

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,5 +31,7 @@ class Square(Rectangle):
         """Sets the value of size"""
         if (type(value) is not int):
             raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
         self.__height = value
