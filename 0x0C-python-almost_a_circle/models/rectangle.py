17d8aa6c50287935f5c828bcf5e552835fc266cc
DicksonWachai <wachaidickson@gmail.com>
2024-02-12 15:09:57 +0300

    Add __str__ method to class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,8 @@ class Rectangle(Base):
             for column in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Returns an informal string representation"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
