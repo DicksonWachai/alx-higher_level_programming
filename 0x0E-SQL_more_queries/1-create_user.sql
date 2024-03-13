b6f886dac243dbd37108c47ae8b74575776f74ca
DicksonWachai <wachaidickson@gmail.com>
2024-03-13 21:41:29 +0300

    script that creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates a user with password
+-- Grants all privileges to user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
