9c953794193fd75b7ff556ed5e75a24a41103eac
DicksonWachai <wachaidickson@gmail.com>
2024-04-08 16:20:58 +0300

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = Number(process.argv[2]);
+if (!isNaN(arg)) {
+  for (let i = 0; i < arg; i++) {
+    let row = '';
+    for (let j = 0; j < arg; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
