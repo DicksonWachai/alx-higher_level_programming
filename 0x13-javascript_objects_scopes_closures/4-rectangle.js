ea01480f8184038f18c18443d91231853a064221
DicksonWachai <wachaidickson@gmail.com>
2024-04-09 15:08:01 +0300

    Correct semistandard errors

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        } else {
-            return undefined;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return undefined;
     }
-    print() {
-        for (let i = 0; i < this.height; i++)
-        {
-            let row = "";
-            for (let j = 0; j < this.width; j++)
-            {
-                row += 'X';
-            }
-            console.log(row);
-            }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
-    double() {
-        this.width = this.width * 2;
-        this.height = this.height * 2;
-    }
-    rotate() {
-        let temp;
-        temp = this.height;
-        this.height = this.width;
-        this.width = temp;
-    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
 }
 module.exports = Rectangle;
