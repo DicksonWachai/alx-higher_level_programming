f10f93187f3ab305a60859c30986112475f137e4
DicksonWachai <wachaidickson@gmail.com>
2024-04-09 16:34:26 +0300

    correct checker error on output

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      return undefined;
-    }
-  }
+const PreviousSquare = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-}
-class Square extends Rectangle {
+class Square extends PreviousSquare {
   constructor (size) {
     super(size, size);
   }
