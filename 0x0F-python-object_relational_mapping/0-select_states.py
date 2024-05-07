9a1ae20e410bc71726089a1c7f06604ed2f7a3be
DicksonWachai <wachaidickson@gmail.com>
2024-05-06 23:31:20 -0700

    Correct checker error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
+'''
+importing MySQLdb and sys for
+the command line args
+'''
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+
+def main():
+    '''
+    The main function that connects to the MySQL database,
+    retrieves data, and prints it.
+    '''
+    try:
+        username = sys.argv[1]
+        passwd = sys.argv[2]
+        database_name = sys.argv[3]
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=passwd,
+            db=database_name)
+        cur = db.cursor()
+        cur.execute("SELECT * from states ORDER BY states.id")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    main()
