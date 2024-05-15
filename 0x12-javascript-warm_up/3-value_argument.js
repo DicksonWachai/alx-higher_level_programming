bff742180db6c705ad1bf71928ecfddc43723b40
Dickson Wachai Nduati <112067606+DicksonWachai@users.noreply.github.com>
2024-05-15 14:01:07 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const i = 2;
-if (process.argv[i]) {
-  console.log(process.argv[i]);
-} else {
-  console.log('No argument');
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const i = 2;
-if (!isNaN(process.argv[i])) {
-  console.log('My number: ' + parseInt(process.argv[i]));
-} else {
-  console.log('Not a number');
-}
